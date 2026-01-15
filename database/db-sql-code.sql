f4682c47242c57da71dcf15f4bd05ba2c59a3055
ammonmerrell <ammonmerrell@outlook.com>
2026-01-15 09:38:36 -0700

    fixed account

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -43,6 +43,6 @@ CREATE TABLE IF NOT EXISTS public.account
 	account_lastname character varying NOT NULL,
 	account_email character varying NOT NULL,
 	account_password character varying NOT NULL,
-	account_type account_type NOT NULL DEFAULT 'CLient'::account_type,
-	CONSTRAINT account_pkey PRIMARY KEY(account_id)
+	account_type account_type NOT NULL DEFAULT 'Client'::account_type,
+	CONSTRAINT account_pkey PRIMARY KEY (account_id)
 );
