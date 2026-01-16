69af88aa688fb7c6f3b81020a49bfc346c921c6c
ammonmerrell <ammonmerrell@outlook.com>
2026-01-16 12:00:37 -0700

    edited data

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,3 +1,5 @@
 --data for account ironman
 INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password)
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+--change account type to Admin
+UPDATE public.account SET account_type = 'Admin' Where account_id = 1;
