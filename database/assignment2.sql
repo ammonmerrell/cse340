2c33511eee332150eedac675493af699e2c456a7
ammonmerrell <ammonmerrell@outlook.com>
2026-01-16 20:43:17 -0700

    added sport data join

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -7,3 +7,7 @@ UPDATE public.account SET account_type = 'Admin' Where account_id = 1;
 DELETE FROM public.account Where account_id =1;
 --Edit gm hummer 
 UPDATE public.inventory SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior');
+--sport data join
+SELECT inventory.inv_make, inventory.inv_model
+FROM public.inventory INNER JOIN public.classification ON classification.classification_id = inventory.classification_id
+AND classification_name = 'Sport'
