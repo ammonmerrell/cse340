e0c7b20b6bd3c79e8e4543d1091448eeb2a245ed
ammonmerrell <ammonmerrell@outlook.com>
2026-01-16 21:26:58 -0700

    copied querys 4 and 6 from assingment2

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -236,3 +236,8 @@ VALUES   (
     'White',
     5
   );
+--Edit gm hummer 
+UPDATE public.inventory SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior');
+--add vehicals in images and thubnails
+UPDATE public.inventory SET inv_image  = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
