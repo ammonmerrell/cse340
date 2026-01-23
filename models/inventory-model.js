c171a78bd5686ee1ac00f05bbf4d458e05a9c662
ammonmerrell <ammonmerrell@outlook.com>
2026-01-23 14:22:14 -0700

    added $1 to end of statement

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -30,7 +30,7 @@ async function getInfoByClassificationId(classification_id) {
       `SELECT * FROM public.inventory AS i 
       JOIN public.classification AS c 
       ON i.classification_id = c.classification_id 
-      WHERE i.inv_id = 2;`,
+      WHERE i.inv_id = $1`,
       [inv_id]
     )
     return data.rows
