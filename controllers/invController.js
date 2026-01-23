3a31af184894dfd1c8295e165b7f1800116e61d4
ammonmerrell <ammonmerrell@outlook.com>
2026-01-23 14:25:00 -0700

    changed function to use getInfo

--- controllers/invController.js
+++ controllers/invController.js
@@ -20,7 +20,7 @@ invCont.buildByClassificationId = async function (req, res, next) {
 }
 invCont.buildByInv_id = async function (req, res, next) {
   const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
+  const data = await invModel.getInfoByClassificationId(classification_id)
   const grid = await utilities.buildClassificationGrid(data)
   let nav = await utilities.getNav()
   const className = data[0].classification_name
