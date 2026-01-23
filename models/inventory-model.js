d90ea9e975a4443b5dcc181a95ddb9ad57260418
ammonmerrell <ammonmerrell@outlook.com>
2026-01-23 13:45:20 -0700

    updated infoById to use inv_id

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -30,8 +30,8 @@ async function getInfoByClassificationId(classification_id) {
       `SELECT * FROM public.inventory AS i 
       JOIN public.classification AS c 
       ON i.classification_id = c.classification_id 
-      WHERE i.classification_id = $1`,
-      [classification_id]
+      WHERE i.inv_id = 2;`,
+      [inv_id]
     )
     return data.rows
   } catch (error) {
