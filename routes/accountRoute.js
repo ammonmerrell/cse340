a0d73ba2a986914621a0093f3c20f446014e311c
ammonmerrell <ammonmerrell@outlook.com>
2026-01-26 15:28:05 -0700

    added accountRoute.js file

---
+++ routes/accountRoute.js
@@ -0,0 +1,6 @@
+// Needed resources
+const express = require("express")
+const router = new express.Router()
+const util = require("../utilities/index")
+const accController = require("../controllers/accCountroller")
+router.get("/account/login", accController.getAccount);
