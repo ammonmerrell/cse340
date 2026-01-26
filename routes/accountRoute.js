0f3e38bc9ae80bd78b659276c3735676fe4dbfa7
ammonmerrell <ammonmerrell@outlook.com>
2026-01-26 16:01:00 -0700

    removed code from controller

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,19 +2,6 @@
 const express = require("express")
 const router = new express.Router()
 const util = require("../utilities/index")
-/* *****
-* Deliver Content View
-* ***** */
-async function  buildLogin(req,res, next) {
-    let nav = await utilities.getNav()
-    res.render("account/login", {
-        title: "login",
-        nav,
-    })    
-}
-
-module.exports = {buildLogin}
-
 const accController = require("../controllers/accountController")
 router.get("/account/login", utilities.handleErrors(accController.getAccount));
 // router.get("/account/login", accController.getAccount);
