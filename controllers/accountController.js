2896c31120a12869061bf82dc9c52060df67795c
ammonmerrell <ammonmerrell@outlook.com>
2026-01-26 16:06:41 -0700

    build accountController

---
+++ controllers/accountController.js
@@ -0,0 +1,14 @@
+const util = require("../utilities/index")
+
+/* *****
+* Deliver Login View
+* ***** */
+async function  buildLogin(req,res, next) {
+    let nav = await utilities.getNav()
+    res.render("account/login", {
+        title: "login",
+        nav,
+    })    
+}
+
+module.exports = {buildLogin}
