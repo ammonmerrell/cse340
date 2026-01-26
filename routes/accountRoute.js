f163d40a4bb7056a078e5f8181b8291cbacddf6a
ammonmerrell <ammonmerrell@outlook.com>
2026-01-26 15:34:28 -0700

    added possible errorcatching middleware

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,4 +3,5 @@ const express = require("express")
 const router = new express.Router()
 const util = require("../utilities/index")
 const accController = require("../controllers/accCountroller")
-router.get("/account/login", accController.getAccount);
+router.get("/account/login", utilities.handleErrors(accController.getAccount));
+// router.get("/account/login", accController.getAccount);
