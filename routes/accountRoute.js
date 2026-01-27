4e2035a4aaf4fbb908a2245ea63123dd13aacde9
ammonmerrell <ammonmerrell@outlook.com>
2026-01-26 17:14:55 -0700

    changed commented section to have correct object

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,5 +4,5 @@ const router = new express.Router()
 const util = require("../utilities/index")
 const accController = require("../controllers/accountController")
 router.get("/account/login", util.handleErrors(accController.buildLogin));
-// router.get("/account/login", accController.getAccount);
+// router.get("/account/login", accController.buildLogin);
 module.exports = router;
