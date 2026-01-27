320d60ca1300baf3748d9f9805e55dfdf649dda6
ammonmerrell <ammonmerrell@outlook.com>
2026-01-27 08:41:56 -0700

    added new route

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,5 +4,5 @@ const router = new express.Router()
 const util = require("../utilities/index")
 const accController = require("../controllers/accountController")
 router.get("/login", util.handleErrors(accController.buildLogin));
-// router.get("/account/login", accController.buildLogin);
+router.get("/register", util.handleErrors(accController.registerUser));
 module.exports = router;
