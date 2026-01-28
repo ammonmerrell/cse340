35b659af593ed2018334832a8f85d59c850d6452
ammonmerrell <ammonmerrell@outlook.com>
2026-01-27 22:10:25 -0700

    add route to new function

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,8 +3,15 @@ const express = require("express")
 const router = new express.Router()
 const util = require("../utilities/index")
 const accController = require("../controllers/accountController")
+const regValidate =require("../utilities/account-validation")
+
 router.get("/login", util.handleErrors(accController.buildLogin));
 router.get("/register", util.handleErrors(accController.registerUser));
 // comment
-router.post('/register', util.handleErrors(accController.registerAccount))
+router.post('/register', 
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
+    util.handleErrors(accController.registerAccount)
+)
+
 module.exports = router;
