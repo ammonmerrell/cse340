51914aee0d29ae2766709635b1cfbd3e931c149d
ammonmerrell <ammonmerrell@outlook.com>
2026-01-27 18:26:10 -0700

    added function

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,5 @@
 const util = require("../utilities/index")
-
+const model = require("../models/account-model")
 /* *****
 * Deliver Login View
 * ***** */
@@ -23,4 +23,35 @@ async function registerUser(req, res, next) {
     })
 }
 
+/* *****
+Process Registration
+* ***** */
+async function registerAccount(req, res){
+    let nav = await util.getNav()
+    const { account_firstname, account_lastname, account_email, account_password} = req.body
+
+    const regResult = await accountModel.registerAccount(
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+
+    if (regResult) {
+        req.flash(
+            "notice",
+            `Congratulations, you\'re registered ${account_firstname}. Please log in.`
+        )
+        res.status(201).render("account/login", {
+            title: "login",
+            nav,
+        })
+    } else {
+        req.flash("notice", "Sorry, the registration failed.")
+        res.status(501).render("account/register", {
+            title: "Registration",
+            nav,
+        })
+    }
+}
 module.exports = {buildLogin, registerUser}
