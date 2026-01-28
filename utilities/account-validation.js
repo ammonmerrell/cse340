05c9722aaf51604acd9c5aec10043816a31b971f
ammonmerrell <ammonmerrell@outlook.com>
2026-01-27 21:41:32 -0700

    added function

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,3 +1,48 @@
 const util = require(".")
   const { body, validationResult } = require("express-validator")
   const validate = {}
+
+/* *****
+* Registeration Data Validation Rules
+* ***** */
+validate.registarionRules = () => {
+    return [
+        //Firstname is req and must be string
+        body("account_firstname")
+          .trim()
+          .escape()
+          .notEmpty()
+          .isLength({ min: 1})
+          .withMessage("Please provide a first name.") //on error this message is sent.
+
+        //lastname is req and must be string
+        body("account_lastname")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isLength({ min: 1})
+            .withMessage("Please provide a last name.") //on error this message is sent.
+        
+        //valid email must exist and not in database
+        body("account_email")
+            .trim()
+            .escape()
+            .notEmpty()
+            .isEmail()
+            .normalizeEmail() //refer to validator.js docs
+            .withMessage("A valid email is required."),
+
+            //Password is req and must be strong
+        body("account_password")
+            .trim()
+            .notEmpty()
+            .isStrongPassword({
+                minLength: 12,
+                minLowercase: 1,
+                minUppercase: 1,
+                minNumbers: 1,
+                minSymbols: 1,
+            })
+            .withMessage("Password does not meet requirements."),
+    ]
+}
