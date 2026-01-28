21f17ef20a8f8a4c3e4e92c2db1535d2c42a1a0b
ammonmerrell <ammonmerrell@outlook.com>
2026-01-27 17:58:22 -0700

    added new route

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,4 +5,6 @@ const util = require("../utilities/index")
 const accController = require("../controllers/accountController")
 router.get("/login", util.handleErrors(accController.buildLogin));
 router.get("/register", util.handleErrors(accController.registerUser));
+// comment
+router.post('/register', util.handleErrors(accController.registerAccount))
 module.exports = router;
