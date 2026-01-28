7f1096ffb5bf81d145046f81278efd9b7944fb5e
ammonmerrell <ammonmerrell@outlook.com>
2026-01-27 18:51:15 -0700

    added space in try block

--- models/account-model.js
+++ models/account-model.js
@@ -5,7 +5,7 @@ const pool = require("../database/")
 * Register new  account 
 * ***** */
 async function registerAccount(account_firstname, account_lastname, account_email, account_password){
-    try{
+    try {
         const sql = "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'client') RETURNING *"
         return await pool.query(sql, [account_firstname, account_lastname, account_email, account_password])
     } catch (error) {
