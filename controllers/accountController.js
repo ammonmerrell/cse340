f714bc6627ac41fb1e4ef45ec7430968ad124a08
ammonmerrell <ammonmerrell@outlook.com>
2026-02-02 13:37:30 -0700

    added the rest of that line

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -97,9 +97,9 @@ async function accountLogin(res, req) {
             delete accountData.account_password
             const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 1000})
             if(process.env.NODE_ENV === 'development') {
-                res.cookie("jwt", accessToken { httpOnly: true, maxAge: 3600 * 1000})
+                res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000})
             } else {
-                res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
+                res.cookie("jwt", accessToken, { httpOnly: true, secure: true, maxAge: 3600 * 1000 })
             }
             return res.redirect("/account/")
         }
