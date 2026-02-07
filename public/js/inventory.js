a77f1b84bf2aa042c48135a7bde57b48ac58e833
ammonmerrell <ammonmerrell@outlook.com>
2026-02-06 17:49:18 -0700

    added quotes for query selector and added fetch

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -1,11 +1,12 @@
 'use strict'
 //Get a list of items in inventory based on classification_id
-let classificationList = document.querySelector('#classificationList')
+let classificationList = document.querySelector("#classificationList")
 classificationList.addEventListener("change", function () {
     let classification_id = classificationList.value
     console.log(`classification_id is: ${classification_id}`)
     let classIdURL = "/inv/getInventory/"+classification_id
-    fetch(function (response) {
+    fetch(classIdURL)
+    .then(function (response) {
         if (response.ok) {
             return response.json();
         }
