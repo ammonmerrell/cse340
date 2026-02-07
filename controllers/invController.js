61b7d1bc0062bf431fceeb8712b0d1722b397848
ammonmerrell <ammonmerrell@outlook.com>
2026-02-06 21:41:42 -0700

    added new function

--- controllers/invController.js
+++ controllers/invController.js
@@ -147,4 +147,36 @@ invCont.getInventoryJSON = async (req, res, next) => {
   }
 }
 
+/* ****************************************
+*  edit inventory Name
+* *************************************** */
+invCont.editInventory = async function (req, res, next) {
+  const inv_id = parseInt(req.params.inv_id)
+  const data = await invModel.getInfoByClassificationId(inv_id)
+  const grid = await utilities.buildIdGrid(data)
+  let nav = await utilities.getNav()
+  const classification = await utilities.buildClassificationList()
+  const carYear = data[0].inv_year
+  const carMake = data[0].inv_make
+  const  carModel = data[0].inv_model
+  const carPrice = data[0].inv_price
+  res.render("./inventory/edit-inventory.ejs", {
+    title:  carMake + "  " + carModel,
+    nav,
+    // data
+    classification,
+    errors: null,
+    inv_id,
+    inv_make,
+    inv_model,
+    inv_year,
+    inv_description,
+    inv_image,
+    inv_thumbnail,
+    inv_price,
+    inv_miles,
+    inv_color,
+    classification_id,
+  })
+}
   module.exports = invCont
