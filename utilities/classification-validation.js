9ff356539843a01f7ccd2caf02fa57f23d5d173c
ammonmerrell <ammonmerrell@outlook.com>
2026-02-07 08:38:26 -0700

    made edit function

--- utilities/classification-validation.js
+++ utilities/classification-validation.js
@@ -82,5 +82,33 @@ validate.checkInvData = async (req, res, next) => {
     next()
 }
 
-
+/* *****
+* Edit Inventory and show results
+* *****/
+validate.checkEditData = async (req, res, next) => {
+    const { inv_make, inv_model, inv_year, inv_desc, inv_price, inv_miles, inv_color, inv_id } = req.body
+    let errors = []
+    errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        let nav = await util.getNav()         
+        // let links = await util.buildInvManager()
+        res.render("inventory/edit-inventory", {
+            errors,
+            title: "Edit: "+ inv_make +" "+inv_model,
+            nav,
+            inv_model,
+            inv_make,
+            inv_year,
+            inv_desc,
+            inv_price,
+            inv_miles,
+            inv_color,
+            inv_id,
+            // classification_name,
+            // links,
+        })
+        return
+    }
+    next()
+}
 module.exports = validate
