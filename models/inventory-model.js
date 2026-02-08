3432dca268e1ef63dfec282a6758488d0e73533e
ammonmerrell <ammonmerrell@outlook.com>
2026-02-07 19:33:11 -0700

    changed order of items

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -62,28 +62,32 @@ async function addClasificationName(classification_name){
 
 async function updateInventoryItem(
   inv_id,
-  inv_make, 
-  inv_model, 
-  inv_year, 
-  inv_description, 
+  inv_make,
+  inv_model,
+  inv_description,
+  inv_image,
+  inv_thumbnail,
   inv_price,
-  inv_miles, 
+  inv_year,
+  inv_miles,
   inv_color,
-  classification_id,
+  classification_id
 ){
   try {
     const sql = 
-    "UPDATE public.inventory SET imv_make = $1, inv_model = $2, inv_year = $3, inv_description = $4, inv_price = $5, inv_miles $6, inv_color = $7, classification_id = $8 WHERE inv_id = $9 RETURNING *"
+    "UPDATE public.inventory SET inv_make = $1, inv_model = $2, inv_description = $3, inv_image = $4, inv_thumbnail = $5, inv_price = $6, inv_year = $7, inv_miles = $8, inv_color = $9, classification_id = $10 WHERE inv_id = $11 RETURNING *"
     const data = await pool.query(sql, [
-    inv_make, 
-    inv_model, 
-    inv_year, 
-    inv_description, 
-    inv_price,
-    inv_miles, 
-    inv_color,
-    classification_id,
-    inv_id,
+    inv_make,
+      inv_model,
+      inv_description,
+      inv_image,
+      inv_thumbnail,
+      inv_price,
+      inv_year,
+      inv_miles,
+      inv_color,
+      classification_id,
+      inv_id,
 ])
 return data.rows[0]
   } catch (error) {
